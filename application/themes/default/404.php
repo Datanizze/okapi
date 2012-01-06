0f1c2bbe66f6fd9a700dc5d846970b6cf2c12d45
johan Liljegren <datanizze@gmail.com>
2012-01-06 05:57:59 +0100

    [add] added 404 page to default theme

---
+++ application/themes/default/404.php
@@ -0,0 +1,8 @@
+<h2>Four o' Four</h2>
+
+<h4 class="error">404 - The page you are looking for does not exist!</h4>
+<p>The requested page 
+http://<?php echo $_SERVER['HTTP_HOST']; ?>
+<strong><?php echo $_SERVER['REQUEST_URI']; ?></strong>
+ could not be found.
+</p>
