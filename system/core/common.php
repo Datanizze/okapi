a79e4035da5ea4c072b575c98eed0e7d26895798
johan Liljegren <datanizze@gmail.com>
2012-01-06 11:48:53 +0100

    [improvement] made generate menu more dynamic (and it's partner functions for getting active menu item, did this so I could use generate_menu for submenus (and any other menu list arrat compatible with the function)

--- system/core/common.php
+++ system/core/common.php
@@ -16,17 +16,16 @@ function path_to($name) {
 	unset($okapi);
 }
 
-function generate_menu($menu_items, $nav_class='okapi-nav') {
+function generate_menu($menu_items, $nav_class='okapi-nav', $active_menu_item = 'active_menu_item', $full_menu=true) {
 	if (isset($menu_items) && is_array($menu_items)) {
 		$class_nav = empty($nav_class) ? '' : ' class="' . $nav_class . '"';
 
-		$active = get_active_menu_item();
-
-		echo "<nav{$class_nav}>";
+		$active = get_active_menu_item($active_menu_item);
+		echo $full_menu ?"<nav{$class_nav}>" : '';
 		foreach($menu_items as $item) {
 			$external = '';
 			$external_icon =''; // TODO: add setting for this in config, you might not want an icon for external links...
-			if ($item['external'] == 1) {
+			if (isset($item['external']) && $item['external'] == 1) {
 				$external_icon = ' <img src="' . path_to('theme') . '/img/external_link_icon.png" alt="opens in new tab" />';
 				$external = ' target="_BLANK"';
 			}
@@ -34,27 +33,27 @@ function generate_menu($menu_items, $nav_class='okapi-nav') {
 			if (isset($item['title']) && !empty($item['title']))
 				$title = " title=\"{$item['title']}\"";
 
-			if ($active != null && strtolower($active) == strtolower($item['text']))
+			if ($active != null && (strtolower($active) == strtolower($item['text']) || (stripos($item['url'], $active) > 0)))
 				echo "<a href=\"{$item['url']}\" class=\"active\"{$title}{$external}>{$item['text']}</a>";
 			else
 				echo "<a href=\"{$item['url']}\"{$title}{$external}>{$item['text']}{$external_icon}</a>";
 		}
-		echo "</nav>";
+		echo $full_menu ? "</nav>" : '';
 	} else {
 		echo "<nav><a href=\"#\">Faulty/No menu</a></nav>";
 	}
 
 }
 
-function set_active_menu_item($item, $force = false) {
-	if(!isset($_REQUEST['active_menu_item']) || $force)
-		$_REQUEST['active_menu_item'] = $item;
+function set_active_menu_item($item, $key='active_menu_item', $force = false) {
+	if(!isset($_REQUEST[$key]) || $force)
+		$_REQUEST[$key] = $item;
 }
 
-function get_active_menu_item() {
+function get_active_menu_item($key='active_menu_item') {
 	$retval = '';
 	// checks if a variable has already been declared, overriding the $active, ugly, I know
-	if (isset($_REQUEST['active_menu_item']))
-		$retval = $_REQUEST['active_menu_item'];
+	if (isset($_REQUEST[$key]))
+		$retval = $_REQUEST[$key];
 	return $retval;
 }
