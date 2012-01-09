13f289b4bec829f21e4b7a5c8e406ff3d852d949
johan Liljegren <datanizze@gmail.com>
2012-01-09 04:08:14 +0100

    [improvement] edited switch, now almost everything goes to the same page

--- application/themes/default/adm/cpage.php
+++ application/themes/default/adm/cpage.php
@@ -1,7 +1,7 @@
 <?php $action = isset($action) ? $action : 'show_all'; ?>
 
 <div class="span-24">
-<?php include('cpage_menu.php');?>
+<?php include('admin_menu.php');?>
 <h2>
 cPage - 
 <?php 
@@ -11,17 +11,12 @@ echo !empty($action_item) ? ' article <strong>' . $action_item . '</strong>' : '
 </h2>
 <?php switch ($action) {
 case 'edit':
-	include('cpage_edit.php');
-	break;
 case 'add':
 	include('cpage_add.php');
 	break;
 case 'delete':
-	include('cpage_del.php');
-	break;
 case 'activate':
 case 'deactivate':
-	include('cpage_activate.php');
 case 'show_all':
 default:
 	include('cpage_all.php');
