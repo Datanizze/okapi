0b18eee65a64cd38c1dd4986461df26ade7104b1
johan Liljegren <datanizze@gmail.com>
2012-01-09 04:07:22 +0100

    [improvement] now sanitizing html shown in artiles table, no need for styling there... changed urls to be more flexible (maybe?), fixed active state to show properly

--- application/themes/default/adm/cpage_all.php
+++ application/themes/default/adm/cpage_all.php
@@ -1,5 +1,6 @@
+<?php echo isset($status) ? "<span class=\"{$status['status']}\">{$status['message']}</span><br /><br />" : ''; ?>
 <?php if (!isset($articles)) { ?>
-<p>No articles found.. Create one?</p>
+<p>No articles found.. <a href="add">Create one?</a></p>
 <?php } else { ?>
 <table class="okapi-table">
 	<tr class="okapi-table-row">
@@ -25,7 +26,8 @@
 			Author
 		</th>
 	</tr>
-<?php foreach($articles as $article) { ?>
+<?php 
+foreach($articles as $article) { ?>
 	<tr class="okapi-table-row">
 		<td class="okapi-table-cell">
 			<a rel="tipsy" href="/page/<?php echo $article['key']; ?>" title="View article"><?php echo $article['key']; ?></a>
@@ -34,15 +36,23 @@
 			<?php echo $article['title']; ?>
 		</td>
 		<td class="okapi-table-cell">
-			<?php echo strlen($article['content']) >60 ? substr($article['content'],0,57) . '<a href="/page/' . $article['key'] . '" rel="tipsy" title="View the rest of this article">...</a>' : $article['content']; ?>
+			<?php
+			$article['content'] = sanitize_html($article['content']);
+			echo strlen($article['content']) >60 ? substr($article['content'],0,57) . '<a href="/page/' . $article['key'] . '" rel="tipsy" title="View the rest of this article">...</a>' : $article['content']; ?>
 		</td>
 		<td class="okapi-table-cell">
 			<?php echo $article['content_type']; ?>
 		</td>
 		<td class="okapi-table-cell">
-			<?php echo $article['active']; ?>
-			<a href="/admin/activate/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/dot-red.png" title="Activate article"/></a>
-			<a href="/admin/deactivate/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/dot-green.png" title="Deactivate article"/></a>
+			<?php 
+			switch ($article['active']) {
+			case 1:
+				echo "<a href=\"deactivate/{$article['key']}\"><img rel=\"tipsy\" src=\"{$tp}/img/icons/dot-green.png\" title=\"deactivate article\"/></a>";
+				break;
+			case 0:
+				echo "<a href=\"activate/{$article['key']}\"><img rel=\"tipsy\" src=\"{$tp}/img/icons/dot-red.png\" title=\"Activate article\"/></a>";
+				break;
+			} ?>
 		</td>
 		<td class="okapi-table-cell">
 			<?php echo $article['created']; ?>
@@ -50,8 +60,9 @@
 		<td class="okapi-table-cell">
 			<?php echo $article['author']; ?>
 		</td>
-		<td class="okapi-table-cell"><a href="/admin/edit/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/document--pencil.png" title="Edit article"/></a></td>
-		<td class="okapi-table-cell"><a href="/admin/remove/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/minus-button.png" title="Remove article"/></a></td>
+		<td class="okapi-table-cell"><a href="edit/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/document--pencil.png" title="Edit article"/></a></td>
+		<td class="okapi-table-cell"><a href="delete/<?php echo $article['key']; ?>"><img rel="tipsy" src="<?php echo $tp; ?>/img/icons/minus-button.png" title="Remove article"/></a></td>
 	</tr>
 <?php }} ?>
 </table>
+<a href="add" class="okapi-button okapi-green right">Add new Article</a>
